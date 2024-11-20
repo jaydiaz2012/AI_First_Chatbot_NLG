f64d209a02c9c570e4302aae9ce03e8216962718
jdiaz <jayremie.diaz@gmail.com>
2024-11-21 02:25:39 +0800

    Update app.py

--- app.py
+++ app.py
@@ -265,31 +265,3 @@ elif options == "SalesX AI":
             prompt = f"Analyze the {forecast}. Provide insights on the trend."
             nlg_response = generate_nlg_response(prompt, forecast)
             st.write("Forecast Sales:", nlg_response)
-
-    #NLP Page
-    def initialize_conversation(prompt):
-        if 'message' not in st.session_state:
-            st.session_state.message = []
-            st.session_state.message.append({"role": "system", "content": System_Prompt})
-
-    initialize_conversation(System_Prompt)
-
-    for messages in st.session_state.message:
-        if messages['role'] == 'system':
-            continue
-        else:
-            with st.chat_message(messages["role"]):
-                st.markdown(messages["content"])
-
-    if user_message := st.chat_input("Ask me more about your forecast!"):
-        with st.chat_message("user"):
-            st.markdown(user_message)
-        st.session_state.message.append({"role": "user", "content": user_message})
-        chat = openai.ChatCompletion.create(
-            model="gpt-4o-mini",
-            messages=st.session_state.message,
-        )
-        response = chat.choices[0].message.content
-        with st.chat_message("assistant"):
-            st.markdown(response)
-        st.session_state.message.append({"role": "assistant", "content": response})
