b5cde7d0e777983475cf4e124059983daf6405f5
jdiaz <jayremie.diaz@gmail.com>
2024-11-20 21:22:12 +0800

    Update app.py

--- app.py
+++ app.py
@@ -267,9 +267,7 @@ if options == "Home":
 # About Us Page
 elif options == "About Me":
     st.title("About Me")
-    My_image = Image.open("images/photo-me1.jpg")
-    my_resized_image = My_image.resize((400,320))
-    st.image(my_resized_image)
+    st.image("images/photo-me1.jpg")
     st.write("I am Jeremie Diaz, an AI builder and programmer.")
     st.write("Don't hesitate to contact me on my LinkedIn and check out my other projects on GitHub!")
     st.write("https://www.linkedin.com/in/jandiaz/")
