0cac5dbcc1ff64b7446b8ff9c5ade818f41e68be
jdiaz <jayremie.diaz@gmail.com>
2024-11-21 11:04:31 +0800

    Update app.py

--- app.py
+++ app.py
@@ -332,12 +332,12 @@ elif options == "Talk to SalesX":
     def initialize_conversation(prompt):
      if 'messagess' not in st.session_state:
          st.session_state.messagess = []
-         st.session_state.messagess.append({"role": "system", "content": System_Prompt_Forecast})
+         st.session_state.messagess.append({"role": "system", "content": System_Prompt})
          chat =  openai.ChatCompletion.create(model = "gpt-4o-mini", messages = st.session_state.messagess, temperature=0.5, max_tokens=1500, top_p=1, frequency_penalty=0, presence_penalty=0)
          response = chat.choices[0].message.content
          st.session_state.messagess.append({"role": "assistant", "content": response})
     
-    initialize_conversation(System_Prompt_Forecast)
+    initialize_conversation(System_Prompt)
     
     for messages in st.session_state.messagess :
       if messages['role'] == 'system' : continue 
